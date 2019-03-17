c5a30e27b510f7be0ec685f6cd6e9a5fd0e2e671
AngelicaOrtiz1290 <angelica.ortizarteaga@gmail.com>
2019-03-17 17:02:58 -0400

    add function getEarthquake to leafletFunction.js

--- ucesrti/platforms/browser/www/js/leafletFunctions.js
+++ ucesrti/platforms/browser/www/js/leafletFunctions.js
@@ -7,3 +7,13 @@ function addPointLinePoly(){
     //add a polygon with 3 end points(i.e. a triangle)
     var myPolygon = L.polygon([[51.509,-0.08],[51.503,-0.06],[51.51,-0.047]],{color:'red',fillColor:'#f03',fillOpacity:0.5}).addTo(mymap).bindPopup("I am a polygon.");
  }
+
+ function getEarthquakes(){
+ 	var client;
+	var earthquakelayer;
+	function getEarthquakes(){
+		client=new XMLHttpRequest();
+		client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
+		client.onreadystatechange = earthquakeResponse;
+		client.send();
+ }
